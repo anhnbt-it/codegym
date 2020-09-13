f28dbce1d0f78b2abadca7129a86a284eb0f3d1b
anhnbt-it <anhnbt.it@gmail.com>
2020-09-13 18:31:03 +0700

    Updated code

--- module-01/week-01-git-html-forms-tables/session-03-pseudo-code-and-flowchart/songuyento.js
+++ module-01/week-01-git-html-forms-tables/session-03-pseudo-code-and-flowchart/songuyento.js
@@ -1,12 +1,11 @@
 for (var i = 0; i < 100; i++) {
-    if(isPrime(i)) console.log(i);
+  if (isPrime(i)) console.log(i);
 }
 
 function isPrime(num) {
-    if(num < 2) return false;
-    for (var i = 2; i < num; i++) {
-        if(num%i==0)
-            return false;
-    }
-    return true;
+  if (num < 2) return false;
+  for (var i = 2; i < num; i++) {
+    if (num % i == 0) return false;
+  }
+  return true;
 }
