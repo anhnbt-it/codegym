bbd8b13c967078ec636c55d89b65b9f2606c9fba
anhnbt-it <anhnbt.it@gmail.com>
2020-09-18 08:18:14 +0700

    Optimization code

--- module-01/week-03-js-loops-arrays-functions/session-15-js-functions-02/exercises-04-exercises-02/exercise-06-check-positive-negative-numbers.js
+++ module-01/week-03-js-loops-arrays-functions/session-15-js-functions-02/exercises-04-exercises-02/exercise-06-check-positive-negative-numbers.js
@@ -1,12 +1,8 @@
 function checkPositiveNegative(n) {
   if (n < 0) {
-    return false;
+    return "NOT positive";
   }
-  return true;
+  return "positive";
 }
 let number = 5;
-if (checkPositiveNegative(number)) {
-  console.log(number + " is Positive numbers");
-} else {
-  console.log(number + " is Negative numbers");
-}
+console.log(checkPositiveNegative(number));
