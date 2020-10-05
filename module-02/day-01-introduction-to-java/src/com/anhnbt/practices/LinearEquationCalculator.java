605c14fcfdb9130984053f77a094ee61d0ef69b1
anhnbt-it <anhnbt.it@gmail.com>
2020-10-05 16:08:33 +0700

    Add Exercise

---
+++ module-02/day-01-introduction-to-java/src/com/anhnbt/practices/LinearEquationCalculator.java
@@ -0,0 +1,28 @@
+package com.anhnbt.practices;
+
+import java.util.Scanner;
+
+public class LinearEquationCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Khởi tạo đối tượng Scanner
+        try {
+            System.out.println("Nhập hằng số a:");
+            double a = scanner.nextDouble(); // Nhập dữ liệu từ bàn phím
+
+            System.out.println("Nhập hằng số b:");
+            double b = scanner.nextDouble();
+
+            if (a != 0) {
+                System.out.printf("Phương trình có nghiệm: %.2f!", (-b / a));
+            } else {
+                if (b == 0) {
+                    System.out.println("Phương trình có vô số nghiệm!");
+                } else {
+                    System.out.println("Phương trình vô nghiệm!");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+        }
+    }
+}
