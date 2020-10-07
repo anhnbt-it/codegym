6461fe389921cefb6e01f5aafcf4cc881de3d9e2
anhnbt-it <anhnbt.it@gmail.com>
2020-10-07 14:56:19 +0700

    Add Practice

--- module-02/day-03-java-arrays/src/com/anhnbt/practices/MinValueInArray.java
+++ module-02/day-03-java-arrays/src/com/anhnbt/practices/MinValueInArray.java
@@ -4,39 +4,21 @@ import java.util.Scanner;
 
 public class MinValueInArray {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int size = 0;
     // declares an array of integers
-    int[] inputArray;
+    int[] numbers = {4, 12, 7, 8, 1, 0, 6, 9};
+    int index = minValue(numbers);
+    System.out.println("The smallest element in the array is: " + numbers[index]);
+  }
 
-    do {
-      System.out.println("Enter a size: ");
-      size = scanner.nextInt();
-      if (size > 20) {
-        System.out.println("Size should not exceed 20");
-      }
-    } while (size > 20);
-
-    // allocates memory for size integers
-    inputArray = new int[size];
-    for (int i = 0; i < inputArray.length; i++) {
-      System.out.println("Enter element at index " + (i + 1) + ": ");
-      inputArray[i] = scanner.nextInt();
-    }
-
-    for (int i = 0; i < inputArray.length; i++) {
-      System.out.println("Element at index " + i + ": " + inputArray[i]);
-    }
-
-    int minValue = inputArray[0];
-    for (int i = 0; i < inputArray.length; i++) {
-      if (minValue > inputArray[i]) {
-        minValue = inputArray[i];
+  public static int minValue(int[] arr) {
+    int minValue = arr[0], index = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (minValue > arr[i]) {
+        minValue = arr[i];
+        index = i;
       }
     }
-    System.out.println("Minimum Value is: " + minValue);
-
-    scanner.close();
+    return (index > 0) ? index : -1;
   }
   
 }
