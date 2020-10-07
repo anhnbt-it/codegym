9397ff4de9e5d7cf8379dd39993c29dd80f166d4
anhnbt-it <anhnbt.it@gmail.com>
2020-10-07 15:32:31 +0700

    Add Practice

---
+++ module-02/day-03-java-arrays/src/com/anhnbt/practices/RemoveElementInArray.java
@@ -0,0 +1,43 @@
+package com.anhnbt.practices;
+
+import java.util.Scanner;
+
+public class RemoveElementInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = {4, 12, 7, 8, 1, 0, 6, 9};
+        System.out.print("value: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println("\nEnter item want to remove: ");
+        int x = sc.nextInt();
+        int index = findIndexOf(numbers, x);
+        if (index != -1) {
+            int[] newArray = removeItemInArray(numbers, index);
+            System.out.print("new value: ");
+            for (int i = 0; i < newArray.length; i++) {
+                System.out.print(newArray[i] + " ");
+            }
+        } else {
+            System.out.println("No item!");
+        }
+    }
+
+    public static int findIndexOf(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (x == arr[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int[] removeItemInArray(int[] arr, int index) {
+        for (int i = index; i < arr.length-1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length-1] = 0;
+        return arr;
+    }
+}
